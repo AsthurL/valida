bfddb5c21a9ab684953f3016bad53fb53843adcf
Asthur Araujo <72561693+AsthurL@users.noreply.github.com>
2025-08-26 10:31:53 -0400

    Add files via upload

---
+++ usuarios.js
@@ -0,0 +1,15 @@
+// Arquivo: usuarios.js
+// Este arquivo armazena a lista de usuários autorizados.
+
+const listaDeUsuarios = [
+  // Formato: { login: "CPF_SEM_PONTOS", senha: "DATA_NASCIMENTO_SEM_PONTOS", filial: "CODIGO_FILIAL" }
+  { login: "1502342103", senha: "27031986", filial: "F01" },
+  { login: "61275174272", senha: "2121977", filial: "F02" },
+  { login: "73164739168", senha: "17071986", filial: "F03" },
+  { login: "1288663145", senha: "10011985", filial: "F94" },
+  { login: "5557257152", senha: "26121995", filial: "F95" },
+  { login: "94842221100", senha: "2111981", filial: "F96" },
+  //
+  // ADICIONE O RESTANTE DOS SEUS USUÁRIOS AQUI, SEGUINDO O MESMO FORMATO
+  //
+];
